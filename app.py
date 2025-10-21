014655b19284a33dfa421615dfd3f340ceac4029
Jakko <j.vreeburg1999@gmail.com>
2025-10-21 19:14:50 +0200

    Disable basic auth and redeploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from openai import AzureOpenAI
 import requests
 import json
-# Triggering new deployment
+# Triggering new deployment, again...
 app = Flask(__name__)
 CORS(app)
 
